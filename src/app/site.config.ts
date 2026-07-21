881bb8ee626004fb9c182f14ea2df5a753b0180a
ts-zx <yxdlzy@outlook.com>
2026-07-21 18:30:26 +0800

    Update site.config.ts

--- src/app/site.config.ts
+++ src/app/site.config.ts
@@ -1,10 +1,10 @@
 export const siteConfig = {
-  name: "我的小站",
+  name: "中出播客",
   description: "记录、思考、分享",
-  author: "Your Name",
-  email: "you@example.com",
-  github: "https://github.com/yourname",
-  url: "https://myblog-xr5.pages.dev",
+  author: "TSZX.",
+  email: "399172250@qq.com",
+  github: "https://github.com/ts-zx",
+  url: "https://tszx.zh.kg",
   language: "zh-CN",
   nav: [
     { href: "/", label: "首页" },
